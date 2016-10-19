d85a437e72f5021eda3c1c4abbb6a9b9869619f2
Tim Combs <tim00combs@gmail.com>
2016-10-19 16:41:24 -0700

    add mkdir functionality, add iterating over array, add creating file writing functionality

--- test/test.js
+++ test/test.js
@@ -1,3 +1,27 @@
+const store = require('../lib/store');
+const assert = require('assert'); 
+
+// const elements = {
+//   hydrogen : {
+//     name: 'hydrogen',
+//     number: 1,
+//     mass: 1,
+//     symbol: 'H'
+//   },
+//   helium : {
+//     name: 'helium',
+//     number: 2,
+//     mass: 4,
+//     symbol: 'He'
+//   },
+//   lithium : {
+//     name: 'lithium',
+//     number: 3,
+//     mass: 7,
+//     symbol: 'Li'
+//   }
+// };
+
 const elements = [
   {
     name: "hydrogen",
@@ -19,3 +43,20 @@ const elements = [
   }
 ];
 
+store.saveFile(elements, 'elements');
+
+
+//the following are the basis of the tests for two filesystem methods
+// store.get('something', (err, 'thing') => {
+//   assert.deepEqual(something, {
+//     name: 'thingName',
+//     breed: 'thingbreed'
+//   }),
+// }
+
+// store.save('something', (err, 'thing') => {
+//   assert.deepEqual(something, {
+//     name: 'thingName',
+//     breed: 'thingbreed'
+//   }),
+// }
