85290a319608c2099e29f9e5859fadb870316e14
Tim Combs <tim00combs@gmail.com>
2016-10-19 14:04:04 -0700

    set up project

---
+++ README.md
@@ -0,0 +1,6 @@
+# File Storage API lab for CodeFellows 401
+
+### Collaborators
+ - Mugsy Carter
+ - Tim Combs
+ - Nathan Hugon
---
+++ test/test.js
@@ -0,0 +1,21 @@
+const elements = [
+  {
+    name: "hydrogen",
+    number: 1,
+    mass: 1,
+    symbol: "H"
+  },
+  {
+    name: "helium",
+    number: 2,
+    mass: 4,
+    symbol: "He"
+  },
+  {
+    name: "lithium",
+    number: 3,
+    mass: 7,
+    symbol: "Li"
+  }
+];
+
